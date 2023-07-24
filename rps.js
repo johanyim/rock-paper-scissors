7120ea9656717f4c5de7f9ba327c81387216e108
johanyim <johanjyyim@gmail.com>
2023-07-24 15:53:17 +0100

    Buttons added, no functionality implemented

--- rps.js
+++ rps.js
@@ -1,21 +1,20 @@
 let win = function(p, c) {
-    return "You played " + p
-    + ". The computer played " + c + ". You Win!";
+    console.log("You played " + p
+    + ". The computer played " + c + ". You Win!");
+    return [1,0]
 }
 
 let lose = function(p, c) {
-    return "You played " + p 
-    + ". The computer played " + c + ". You Lose!";
+    console.log("You played " + p 
+    + ". The computer played " + c + ". You Lose!");
+    return [0,1]
 }
 
 let tie = function(p, c) {
-    return "You played " + p
-    + ". The computer played " + c + ". You Tie!";
+    console.log("You played " + p
+    + ". The computer played " + c + ". You Tie!");
+    return [0,0]
 }
-
-
-
-
 
 function playRound(playerSelection, computerSelection, 
     win, lose, tie) {
@@ -35,17 +34,14 @@ function playRound(playerSelection, computerSelection,
     //unrecognized
     if(!moveMap.has(playerSelection.toLowerCase())) {
         console.log("unexpected outcome")
-        return -1
+        return [0,0]
     }
     
-
     let p = moveMap.get(playerSelection.toLowerCase());
     let c = moveMap.get(computerSelection.toLowerCase());
 
     return outcome[p][c](playerSelection.toLowerCase(),computerSelection.toLowerCase())
 }
-
-
 
 function getComputerChoice() {
     choice = Math.floor(Math.random() * 3);
@@ -56,7 +52,6 @@ function getComputerChoice() {
     }
 }
 
-
 function getPlayerChoice() { 
     input = prompt("Your move (Rock, Paper, Scissors)");
 
@@ -65,5 +60,44 @@ function getPlayerChoice() {
 
 }
 
+// const timeout = async ms => new Promise(res => setTimeout(res, ms));
+// let next = false; // this is to be changed on user input
 
-console.log(playRound(getPlayerChoice(), getComputerChoice(), win, lose, tie))
+// async function waitUserInput() {
+//     while (next === false) await timeout(50); // pauses script
+//     next = false; // reset var
+// }
+
+
+function play(move) {
+    console.log("Played " + move)
+}
+
+
+
+
+score = [0,0]
+
+async function game() {
+    let score = [0,0];
+    do {
+        outcome = await playRound(getPlayerChoice(), getComputerChoice(), win, lose, tie)
+        // vector addition operation
+        score[0] += outcome[0];
+        score[1] += outcome[1];
+
+        console.log("SCORE:", score)
+        console.log("game has ended");
+
+        console.log(score)
+    } while(score[0] < 1 && score[1] < 1);
+
+
+    game()
+    return score;
+    
+}
+
+// console.log(playRound(getPlayerChoice(), getComputerChoice(), win, lose, tie))
+
+// console.log([3,4] + [5,6])
